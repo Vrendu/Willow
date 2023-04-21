3fd01408ca86e1e72433ae48c9c206c4f48e355c
Vamshi Renduchintala <vrenduchintala17@gmail.com>
2023-04-21 16:14:40 -0700

    checking that User model works with authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_21_205505) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_21_221844) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_21_205505) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "session_token"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["password_digest"], name: "index_users_on_password_digest", unique: true
   end
