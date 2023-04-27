245862e72ebfe12979a1301b367fabd346260261
Vamshi Renduchintala <vrenduchintala17@gmail.com>
2023-04-27 09:43:18 -0700

    styling of listings index

--- frontend/src/components/Listings/Listings.js
+++ frontend/src/components/Listings/Listings.js
@@ -2,7 +2,11 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { fetchListings, getListings } from '../../store/listingsActions';
-import "./Listings.css"
+import "./Listings.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Navigation } from 'swiper';
+import 'swiper/css/bundle';
 
 const Listings = () => {
     const dispatch = useDispatch();
@@ -15,20 +19,31 @@ const Listings = () => {
     
 
     return (
-        <div>
-            <h2>Listings</h2>
-            {console.log(listings, "listings component")}
+        <div className="container">
+            <Swiper
+                modules={[Navigation]}
+                spaceBetween={50}
+                slidesPerView={3}
+                navigation
+                // pagination={{ clickable: true }}
+                // scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+            >
             {listings.length > 0 && listings.map((listing, index) => (
+                <SwiperSlide>
                 <div key={listing.id} className="card">
                     <img src={listing.photos[0]} alt={listing.title} />
                     <div className="card-body">
-                        <h3>{listing.title}</h3>
+                        <h3>${Math.floor(listing.price)}</h3>
                         {/* <p>{listing.description}</p> */}
-                        <p>Price: ${listing.price}</p>
-                        <p>Square Feet: {listing.square_feet}</p>
+                        <p>{listing.bedrooms} bds | {listing.bathrooms} ba | {listing.square_feet} sqft</p>
+                        <p>{listing.address}</p>
                     </div>
                 </div>
+                </SwiperSlide>
             ))}
+            </Swiper>
         </div>
     );
 };
