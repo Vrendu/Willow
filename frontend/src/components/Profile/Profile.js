db9a524b3211db9818392dc130de5546c7ada961
Vamshi Renduchintala <vrenduchintala17@gmail.com>
2023-05-25 16:53:38 -0700

    confirmation modal to delete tour booking

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,51 +1,66 @@
 import React, { useEffect, useState } from "react";
 import "./Profile.css";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom";
 import { getFavorites } from "../../store/favoritesActions";
-import { fetchBookings } from "../../store/bookingsActions";
+import { fetchBookings, deleteBooking } from "../../store/bookingsActions";
 import { fetchUser } from "../../store/session";
 import { fetchListings, getListings } from "../../store/listingsActions";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import 'swiper/css/bundle';
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Mousewheel } from "swiper"; 
-import { FaCalendarAlt, FaClock } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FaCalendarAlt, FaClock, FaTimes } from "react-icons/fa";
 
 function Profile() {
+    const [activeTab, setActiveTab] = useState("bookings");
     const dispatch = useDispatch();
     const currentUser = useSelector((state) => state.session.user);
     const history = useHistory();
-    if (!currentUser) {
-        history.push("/");
-    }
+
     const favorites = useSelector(getFavorites);
     const bookings = useSelector((state) => state.bookings);
     const listings = useSelector(getListings);
+
+    const [showConfirmation, setShowConfirmation] = useState(false);
+    const [bookingToDelete, setBookingToDelete] = useState(null);
 
     useEffect(() => {
         dispatch(fetchBookings());
         dispatch(fetchListings());
         if (currentUser) {
-            dispatch(fetchUser(currentUser.id))
+            dispatch(fetchUser(currentUser.id));
         }
     }, [dispatch]);
 
-    if (!currentUser) {
-        return null;
-    }
-    return (
-        <div>
-            <div className="profile-container">
+    const handleDeleteBooking = (bookingId) => {
+        setBookingToDelete(bookingId);
+        setShowConfirmation(true);
+    };
 
-            
-                <h2>{currentUser.username}'s Profile</h2>
-                
-                <h2>Upcoming Tours</h2>
+    const handleConfirmDelete = () => {
+        // Implement your logic to delete the booking with the given bookingId
+        // For example, you can dispatch an action to delete the booking from the store
+        console.log("Delete booking with id: ", bookingToDelete);
+        dispatch(deleteBooking(bookingToDelete));
+
+        // Reset the confirmation state
+        setBookingToDelete(null);
+        setShowConfirmation(false);
+    };
+
+    const handleCancelDelete = () => {
+        // Reset the confirmation state
+        setBookingToDelete(null);
+        setShowConfirmation(false);
+    };
+
+    function renderBookings() {
+        if (Object.keys(bookings).length === 0) {
+            return <h1 className="no-bookings-message">No Tours Currently Booked</h1>;
+        }
+        return (
+            <div className="bookings">
                 {Object.keys(bookings).map((bookingKey) => {
                     const booking = bookings[bookingKey];
                     if (booking && currentUser && booking.user_id === currentUser.id) {
-                        // Find the corresponding listing for the booking
                         const listing = listings.find((listing) => listing.id === booking.listing_id);
                         if (listing && listing.photos.length > 0) {
                             const formattedTime = new Date(booking.time).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
@@ -62,11 +77,16 @@ function Profile() {
                                         </p>
                                         <br />
                                         <p>{listing.address}, {listing.city}, {listing.state} {listing.zip_code}</p>
+                                        <br />
+                                        <p className="delete-booking">
+                                            <FaTimes className="delete-icon" onClick={() => handleDeleteBooking(booking.id)} />
+                                            <span className="cancel-booking">Cancel Booking</span>
+                                        </p>
+                                        
                                     </div>
                                 </div>
                             );
                         } else {
-                            // Handle the case when the listing or photos are not available
                             return (
                                 <div className="upcoming-tour" key={booking.id}>
                                     <div className="tour-details">
@@ -79,62 +99,85 @@ function Profile() {
                                         </p>
                                         <p>Listing details not available</p>
                                     </div>
+                                    <p className="delete-booking" onClick={() => handleDeleteBooking(booking.id)}>
+                                        <FaTimes className="delete-icon" />
+                                        <span>Cancel Booking</span>
+                                    </p>
+                                    
                                 </div>
                             );
                         }
                     }
                 })}
             </div>
+        );
+    }
 
-            <h2 className="favorites-header">Your Favorites</h2>
-            <div className="container">
-                <Swiper
-                    modules={[Navigation, Mousewheel]}
-                    spaceBetween={50}
-                    slidesPerView={3}
-                    navigation
-                    mousewheel
-                >
-            {Object.keys(favorites).map((favoriteKey) => {
-                const favorite = favorites[favoriteKey];
-                if (favorite && currentUser && favorite.user_id === currentUser.id) {
-                    return (
-                        <div>
-                            {listings.map((listing) => {
-                                if (listing && favorite.listing_id === listing.id) {
-                                    return (
-                                        <SwiperSlide key={listing.id}>
-                                            <Link to={`/listings/${listing.id}`} className="card-link">
-                                                <div className="card">
-                                                    <img src={listing.photos[0]} alt={listing.title} />
-                                                    <div className="card-body">
-                                                        <h3>${Math.floor(listing.price).toLocaleString()}</h3>
-                                                        <p>
-                                                            {listing.bedrooms} bd <span className="lighter">|</span>{" "}
-                                                            {listing.bathrooms} ba{" "}
-                                                            <span className="lighter">|</span>{" "}
-                                                            {listing.square_feet} sqft
-                                                        </p>
-                                                        <p>{listing.address}</p>
-                                                    </div>
-                                                </div>
-                                            </Link>
-                                        </SwiperSlide>
-                                    )
-                                }
-                            }
-                            )}
+    function renderFavorites() {
+        return (
+            <div className="favorites">
+                {Object.keys(favorites).map((favoriteKey) => {
+                    const favorite = favorites[favoriteKey];
+                    if (favorite && currentUser && favorite.user_id === currentUser.id) {
+                        const listing = listings.find((listing) => listing.id === favorite.listing_id);
+                        if (listing && listing.photos.length > 0) {
+                            return (
+                                <Link to={`/listings/${listing.id}`} className="fcard-link" key={favorite.id}>
+                                    <div className="fcard">
+                                        <img src={listing.photos[0]} alt={listing.title} />
+                                        <div className="card-body">
+                                            <h3>${Math.floor(listing.price).toLocaleString()}</h3>
+                                            <p>
+                                                {listing.bedrooms} bd <span className="lighter">|</span>{" "}
+                                                {listing.bathrooms} ba{" "}
+                                                <span className="lighter">|</span>{" "}
+                                                {listing.square_feet} sqft
+                                            </p>
+                                            <p>{listing.address}</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            );
+                        }
+                    }
+                })}
+            </div>
+        );
+    }
+
+    if (!currentUser) {
+        return null;
+    }
+
+    return (
+        <div className="profile-page">
+            <div className="profile-container">
+                <div className="tab-navigation">
+                    <span className="bookings-button" onClick={() => setActiveTab("bookings")}>
+                        Upcoming Tours
+                    </span>
+                    <span className="favorites-button" onClick={() => setActiveTab("favorites")}>
+                        Your Favorites
+                    </span>
+                </div>
+                {showConfirmation && (
+                    <div className="confirmation-modal">
+                        <div className="confirmation-content">
+                            <p>Are you sure?</p>
+                            <div className="confirmation-buttons">
+
+                                <button onClick={handleConfirmDelete}>Yes</button>
+                                <button onClick={handleCancelDelete}>No</button>
+                            </div>
                         </div>
-                    )
-                }
-            }
-            )}
-            </Swiper>
+                    </div>
+                )}
+                {activeTab === "bookings" && renderBookings()}
+                {activeTab === "favorites" && renderFavorites()}
+                
+            </div>
         </div>
-            
-        </div>
-    )
-
+    );
 }
 
 export default Profile;
