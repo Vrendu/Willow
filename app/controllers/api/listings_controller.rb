04963a5d1cf8f570a45eeca4bf7384afea0444f1
Vamshi Renduchintala <vrenduchintala17@gmail.com>
2023-10-09 11:35:20 -0700

    adjust range

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -20,7 +20,7 @@ wrap_parameters :listing, include: Listing.attribute_names + [:photos]
             user_location = [user_latitude, user_longitude]
 
             # Listings within a 100-kilometer radius of the user's location
-            @listings = Listing.near(user_location, 100)
+            @listings = Listing.near(user_location, 200)
         end
         render :index
     end
@@ -40,19 +40,6 @@ wrap_parameters :listing, include: Listing.attribute_names + [:photos]
             render json: { errors: @listing.errors.full_messages }, status: :unprocessable_entity
         end
     end
-
-    # def search
-    #     if params[:search].present?
-    #         search_query = "%#{params[:search]}%"
-    #         @listings = Listing.where(
-    #         "city ILIKE ? OR state ILIKE ? OR address ILIKE ?",
-    #         search_query, search_query, search_query
-    #         )
-    #     else
-    #         @listings = Listing.all
-    #     end
-    #     render :index
-    # end
 
     def edit
         @listing = Listing.find(params[:id])
