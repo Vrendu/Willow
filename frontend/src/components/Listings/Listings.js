07ade89611ee0369ad4c20323d72ad0496e84ac5
Vamshi Renduchintala <vrenduchintala17@gmail.com>
2023-10-08 21:38:01 -0700

    implemented geocoding to render listings based on user location

--- frontend/src/components/Listings/Listings.js
+++ frontend/src/components/Listings/Listings.js
@@ -13,113 +13,19 @@ import { clearAllListings } from '../../store/listingsActions';
 const Listings = () => {
     const dispatch = useDispatch();
     const listings = useSelector(getListings);
-   // const [userLocation, setUserLocation] = useState(null);
-    //const [userCoordinates, setUserCoordinates] = useState(null);
-    const stateToAbbr = { 
-        "Alabama": "AL",
-        "Alaska": "AK",
-        "American Samoa": "AS",     
-        "Arizona": "AZ",
-        "Arkansas": "AR",
-        "California": "CA",
-        "Colorado": "CO",
-        "Connecticut": "CT",
-        "Delaware": "DE",
-        "District Of Columbia": "DC",
-        "Federated States Of Micronesia": "FM",
-        "Florida": "FL",
-        "Georgia": "GA",
-        "Guam": "GU",
-        "Hawaii": "HI",
-        "Idaho": "ID",
-        "Illinois": "IL",
-        "Indiana": "IN",
-        "Iowa": "IA",
-        "Kansas": "KS",
-        "Kentucky": "KY",
-        "Louisiana": "LA",
-        "Maine": "ME",
-        "Marshall Islands": "MH",
-        "Maryland": "MD",
-        "Massachusetts": "MA",
-        "Michigan": "MI",
-        "Minnesota": "MN",
-        "Mississippi": "MS",
-        "Missouri": "MO",
-        "Montana": "MT",
-        "Nebraska": "NE",
-        "Nevada": "NV",
-        "New Hampshire": "NH",
-        "New Jersey": "NJ",
-        "New Mexico": "NM",
-        "New York": "NY",
-        "North Carolina": "NC",
-        "North Dakota": "ND",
-        "Northern Mariana Islands": "MP",
-        "Ohio": "OH",
-        "Oklahoma": "OK",
-        "Oregon": "OR",
-        "Palau": "PW",
-        "Pennsylvania": "PA",
-        "Puerto Rico": "PR",
-        "Rhode Island": "RI",
-        "South Carolina": "SC",
-        "South Dakota": "SD",
-        "Tennessee": "TN",
-        "Texas": "TX",
-        "Utah": "UT",
-        "Vermont": "VT",
-        "Virgin Islands": "VI",
-        "Virginia": "VA",
-        "Washington": "WA",
-        "West Virginia": "WV",
-        "Wisconsin": "WI",
-        "Wyoming": "WY"
-        };
     
     useEffect(() => {    
         dispatch(clearAllListings());     
     }, [dispatch]);
 
-    // want to get user location from IP address, and then fetch listings based on that location
     useEffect(() => {
-        // Make an API request to get user location based on IP
-        // axios.get('https://ipinfo.io/json?token=7cf3db293dbbd5')
-        //     .then((response) => {
-        //         // Extract location data from the response
-        //         const { city, region, postal } = response.data;
-        //         //console.log("response", response.data)
-        //         setUserLocation(`${city}, ${region} ${postal}`);
-        //         console.log("user location", userLocation);
-        //         console.log("region", region);
-        //         // Use geocoding API to convert user's location to coordinates
-        //         // const geocoder = new window.google.maps.Geocoder();
-        //         // geocoder.geocode({ address: userLocation }, (results, status) => {
-        //         //     if (status === "OK") {
-        //         //         const { lat, lng } = results[0].geometry.location;
-        //         //         setUserCoordinates({ lat, lng });
-        //         //         console.log("results ", results[0].geometry.location)
-        //         //         console.log("user coordinates", userCoordinates);
-        //         //     } else {
-        //         //         console.error("Geocoding failed:", status);
-        //         //     }
-        //         // });
-
-                
-                dispatch(fetchListings());
-                
-           // })
-            // .catch((error) => {
-            //     console.log('Error fetching user location:', error);
-            // });
-        
+        dispatch(fetchListings());
     }, [dispatch]);
    
 
     if (!listings) {
         return null;
     }
-
     return (
         <> 
         <span className="recently-added-text"> Listings Near You </span>
